e7be242e0d984e63105a488b16b0bda800f4d898
sujalchand <sujalchand2526@gmail.com>
2026-09-12 14:52:12 +1200

    add CustomerController with endpoints for retrieving and creating customers

---
+++ customer-service/src/main/java/nz/ac/aut/comp713/customer_service/controller/CustomerController.java
@@ -0,0 +1,43 @@
+package nz.ac.aut.comp713.customer_service.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import nz.ac.aut.comp713.customer_service.model.Customer;
+import nz.ac.aut.comp713.customer_service.service.CustomerService;
+
+@RestController
+@RequestMapping("/api/v1/customers")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public List<Customer> getAllCustomers() {
+        return customerService.getAllCustomers();
+    }
+
+    @GetMapping("/{id}")
+    public Customer getCustomerById(@PathVariable Long id) {
+        return customerService.getCustomerById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer createCustomer(@Valid @RequestBody Customer customer) {
+        return customerService.createCustomer(customer);
+    }
+}
