d8a2015173a8512e1dbd9eb3e5eb64f8b0e5fd4b
sujalchand <sujalchand2526@gmail.com>
2026-09-15 14:52:28 +1200

    add TaroType entity and repository for managing TaroType data

---
+++ customer-service/src/main/java/nz/ac/aut/comp713/customer_service/model/TaroType.java
@@ -0,0 +1,69 @@
+package nz.ac.aut.comp713.customer_service.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "taro_type")
+public class TaroType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "normalized_name", nullable = false, unique = true)
+    private String normalizedName;
+
+    private String description;
+
+    @Column(name = "standard_price", nullable = false)
+    private BigDecimal standardPrice;
+
+    public TaroType() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNormalizedName() {
+        return normalizedName;
+    }
+
+    public void setNormalizedName(String normalizedName) {
+        this.normalizedName = normalizedName;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getStandardPrice() {
+        return standardPrice;
+    }
+
+    public void setStandardPrice(BigDecimal standardPrice) {
+        this.standardPrice = standardPrice;
+    }
+}
---
+++ customer-service/src/main/java/nz/ac/aut/comp713/customer_service/repository/TaroTypeRepository.java
@@ -0,0 +1,9 @@
+package nz.ac.aut.comp713.customer_service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import nz.ac.aut.comp713.customer_service.model.TaroType;
+
+public interface TaroTypeRepository extends JpaRepository<TaroType, Long> {
+
+}
