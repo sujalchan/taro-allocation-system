83666326e9c17219cc30711f4736f3082b1f5e61
sujalchand <sujalchand2526@gmail.com>
2026-09-16 01:38:49 +1200

    update methods to AllocationItemRepository and WeeklyAllocationRepository for data retrieval

--- allocation-service/src/main/java/nz/ac/aut/comp713/allocation_service/repository/AllocationItemRepository.java
+++ allocation-service/src/main/java/nz/ac/aut/comp713/allocation_service/repository/AllocationItemRepository.java
@@ -3,6 +3,9 @@ package nz.ac.aut.comp713.allocation_service.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import nz.ac.aut.comp713.allocation_service.model.AllocationItem;
+import java.util.List;
 
 public interface AllocationItemRepository extends JpaRepository<AllocationItem, Long> {
+
+    List<AllocationItem> findByWeeklyAllocationId(Long weeklyAllocationId);
 }
--- allocation-service/src/main/java/nz/ac/aut/comp713/allocation_service/repository/WeeklyAllocationRepository.java
+++ allocation-service/src/main/java/nz/ac/aut/comp713/allocation_service/repository/WeeklyAllocationRepository.java
@@ -1,8 +1,12 @@
 package nz.ac.aut.comp713.allocation_service.repository;
 
+import java.time.LocalDate;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import nz.ac.aut.comp713.allocation_service.model.WeeklyAllocation;
 
 public interface WeeklyAllocationRepository extends JpaRepository<WeeklyAllocation, Long> {
+
+    boolean existsByCustomerIdAndWeekStart(Long customerId, LocalDate weekStart);
 }
